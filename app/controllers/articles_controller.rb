8bb45aa69b570445d6d488f74aaf5944b409829c
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-10-30 20:27:44 +0000

    create relationship between article and comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,15 +13,34 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    if @article.save!
+    if @article.save
       redirect_to @article
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:d])
+    @article.destroy
+    redirect_to root_path, status: :see_other
+  end
 
   private
+
   def article_params
     params.require(:article).permit(:title, :body)
   end
